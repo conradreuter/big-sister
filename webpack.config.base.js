9d675cdaf6d68c6b34a1feb775f998aa5383f3d0
Conrad Reuter <conrad.reuter@enavo.com>
2018-01-25 16:10:55 +0100

    integrate ui

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,11 +1,17 @@
-const path = require('path')
+const glob = require('glob')
 const HtmlPlugin = require('html-webpack-plugin')
+const _ = require('lodash')
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: [
-    'babel-polyfill',
-    './src/node_modules/@big-sister-ui/core',
-  ],
+  context: path.resolve(__dirname, 'src/node_modules/@big-sister'),
+  entry: (
+    _(glob.sync('*/ui/', { cwd: path.resolve(__dirname, 'src/node_modules/@big-sister') }))
+    .map(dir => [path.dirname(dir), `./${dir}`])
+    .fromPairs()
+    .value()
+  ),
   module: {
     rules: [
       {
@@ -28,15 +34,21 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, './dist/node_modules/@big-sister-ui/core'),
-    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, './dist/node_modules/@big-sister'),
+    filename: '[name]/ui/index.js',
   },
   performance: {
     hints: false
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      filename: 'core/ui/common.js',
+      name: 'common',
+    }),
     new HtmlPlugin({
-      template: path.resolve(__dirname, 'src/node_modules/@big-sister-ui/core/index.html'),
+      filename: 'core/ui/index.html',
+      inject: false,
+      template: 'core/ui/index.html',
     }),
   ],
   resolve: {
